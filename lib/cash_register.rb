3b7407ac52e559290ea9040fd2637d635cdc558c
Samantha Meason <sbmeason@gmail.com>
2020-02-16 22:38:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,5 @@
 class CashRegister
-  attr_accessor :total, :discount, :price, :items, :last_transaction_amount
+  attr_accessor :total, :discount, :price, :items, :last_transaction
 
   def initialize(discount = 0)
     @total = 0
@@ -32,7 +32,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    @total -= @price
+    @total -= @last_transaction 
   end
 
 end
